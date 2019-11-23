57a22c7704903cec51a78aa40b9ba90dd6ae1743
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-11-23 19:39:37 +0100

    call frontendmaster pet api

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 
+import { ANIMALS } from '@frontendmasters/pet';
+
 const SearchParams = () => {
   const [location, setlocation] = useState('Seatle, WA');
+  const [animal, setAnimal] = useState('dog');
   return (
     <div className="search-params">
       <form>
@@ -14,6 +17,24 @@ const SearchParams = () => {
             placeholder="location"
             onChange={e => setlocation(e.target.value)}
           />
+        </label>
+
+        <label htmlFor="animal">
+          animal
+          <select
+            name="animal"
+            id="animal"
+            value={animal}
+            onBlur={e => setAnimal(e.target.value)}
+            onChange={e => setAnimal(e.target.value)}
+          >
+            <option value="">All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
