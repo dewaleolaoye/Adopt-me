3ef48004636ff56c6270dad84aa1f7539c4b38e6
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-11-23 17:22:49 +0100

    pure react

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
-const a = 'me';
+import React from 'react';
+import { render } from 'react-dom';
+
+import Pet from './Pet';
+
+const App = () => {
+  return React.createElement('div', { id: 'something-important' }, [
+    React.createElement('h1', {}, 'Adopt Me!'),
+
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Havanese'
+    }),
+    React.createElement(Pet, {
+      name: 'Popper',
+      animal: 'Bird',
+      breed: 'Cockatiel'
+    }),
+    React.createElement(Pet, { name: 'Doink', animal: 'Cat', breed: 'Mixed' })
+  ]);
+};
+
+render(React.createElement(App), document.getElementById('root'));
