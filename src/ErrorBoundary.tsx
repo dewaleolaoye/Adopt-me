789dc6c53a9c19cf711f274c56327c938f3d4891
dewaleolaoye <adewaleolaoye21@gmail.com>
2019-12-05 19:47:45 +0100

    type Errorboundary

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, ErrorInfo } from 'react';
 import { Link, Redirect } from '@reach/router';
 
 class ErrorBoundary extends Component {
-  state = { hasError: false, redirect: false };
+  public state = { hasError: false, redirect: false };
 
-  static getDerivedStateFromError() {
+  public static getDerivedStateFromError() {
     return { hasError: true };
   }
 
-  componentDidCatch(error, info) {
+  public componentDidCatch(error: Error, info: ErrorInfo) {
     // eslint-disable-next-line no-console
     console.error('Error Boundary caught error', error, info);
   }
 
-  componentDidUpdate() {
+  public componentDidUpdate() {
     if (this.state.hasError) {
       setTimeout(() => this.setState({ redirect: true }), 5000);
     }
   }
-  render() {
+  public render() {
     if (this.state.redirect) {
       return <Redirect to="/" />;
     }
